8698059ecfda9b0c5fddf3647af2b5ba2ce44597
Omar Jiménez <omarjimdiz-coder@github.com>
2021-04-14 17:05:30 -0500

    Preload terminado

---
+++ app.js
@@ -0,0 +1,7 @@
+
+window.addEventListener('load', () => {
+    const preload = document.querySelector('.preload');
+    setTimeout(() => {
+        preload.classList.add('preload-finish');
+    }, 4000)
+});
